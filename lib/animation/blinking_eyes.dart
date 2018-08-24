83ee563d755849642cf021f97f706148baaa6739
idan <studioidan@gmail.com>
2018-08-24 13:24:41 +0300

    example project added

---
+++ lib/animation/blinking_eyes.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class BlinkingEyes extends StatefulWidget {
+  final height;
+  final color;
+  final duration = 200;
+
+  BlinkingEyes(this.height, this.color);
+
+  @override
+  BlinkingEyesState createState() => new BlinkingEyesState();
+}
+
+class BlinkingEyesState extends State<BlinkingEyes>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: widget.duration));
+
+    _sizeAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) _controller.reverse();
+        // if (status == AnimationStatus.dismissed) _controller.forward();
+      })
+      ..addListener(() => setState(() {}));
+
+    blink();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double circleSize = widget.height * 0.4;
+
+    return new Center(
+      child: new Container(
+        height: widget.height,
+        width: widget.height,
+        // decoration: new BoxDecoration(color: Colors.blueGrey),
+        child: new Center(
+            child: RotatedBox(
+          quarterTurns: 2,
+          child: new Transform(
+              transform: Matrix4.identity()..scale(1.0, _sizeAnimation.value),
+              child: new Row(
+                children: <Widget>[
+                  getCircle(circleSize, widget.color),
+                  getCircle(circleSize, widget.color),
+                ],
+              )),
+        )),
+      ),
+    );
+  }
+
+  void blink() {
+    _controller.forward();
+    new Timer(Duration(milliseconds: new Random().nextInt(2000)), () {
+      blink();
+    });
+  }
+
+  Widget getCircle(double height, Color c) {
+    var circleSize = height * 0.4;
+    return new Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Stack(
+          children: <Widget>[
+            Container(
+                height: height,
+                width: height,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: c,
+                )),
+            Positioned(
+              left: height / 2 - circleSize / 2,
+              top: height / 2 - circleSize / 2,
+              child: Container(
+                  height: circleSize,
+                  width: circleSize,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.black)),
+            )
+          ],
+        ));
+  }
+}
--- lib/flutter_animated_loaders.dart
+++ lib/flutter_animated_loaders.dart
@@ -1,10 +1,15 @@
 library flutter_animated_loaders;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_animated_loaders/animation/Pulse.dart';
+import 'package:flutter_animated_loaders/animation/blinking_eyes.dart';
+import 'package:flutter_animated_loaders/animation/pulse.dart';
 
 class FlutterLoaders {
-  static Widget Pulse(Icon icon, Color rippleColor) {
+  static Widget pulse(Icon icon, Color rippleColor) {
     return PulseLoader(icon, rippleColor);
+  }
+
+  static Widget blinkingEyes(double size, Color color) {
+    return BlinkingEyes(size, color);
   }
 }
